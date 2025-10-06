c44b1d34f07df3f4c0e39ac8fb9ad895e44311f1
Pawan Bharambe <121633721+pawan122003@users.noreply.github.com>
2025-10-06 21:40:38 +0530

    chore(codeql): add minimal python package and test

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+"""GCP IAM Threat Detection Lab - Main Application Package."""
+
+__version__ = "0.1.0"
+__author__ = "Pawan Bharambe"
+
+def hello():
+    """Return a greeting message."""
+    return "Hello from GCP IAM Threat Detection Lab!"
